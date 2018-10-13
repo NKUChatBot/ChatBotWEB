35f2edba2e69cf91baae5f9440f510839cf82cc9
Chinese-BruceWayne <1184249450@qq.com>
2018-10-13 16:37:39 +0800

    add cusomize user model ==> add field in_cj_block

--- User/models.py
+++ User/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+class User(AbstractUser):
+    in_cj_block = models.BooleanField(default=True)
